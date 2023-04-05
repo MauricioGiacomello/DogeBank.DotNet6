0bfc19cb43d6b9f594a09249d6fb5016c52aa6ab
MauricioGiacomello <giacomerro@gmail.com>
2023-04-05 09:51:33 -0300

    Removido Vs code

--- DogeBank/DOB.ApiDotNet6.Ioc/obj/Debug/net6.0/DOB.ApiDotNet6.Ioc.AssemblyInfo.cs
+++ DogeBank/DOB.ApiDotNet6.Ioc/obj/Debug/net6.0/DOB.ApiDotNet6.Ioc.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("DOB.ApiDotNet6.Ioc")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("DOB.ApiDotNet6.IoC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("DOB.ApiDotNet6.Ioc")]
-[assembly: System.Reflection.AssemblyTitleAttribute("DOB.ApiDotNet6.Ioc")]
+[assembly: System.Reflection.AssemblyProductAttribute("DOB.ApiDotNet6.IoC")]
+[assembly: System.Reflection.AssemblyTitleAttribute("DOB.ApiDotNet6.IoC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
