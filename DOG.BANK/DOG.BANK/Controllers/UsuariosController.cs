fad97e950fc9289d9de73fe782cb8f714cc2bfbf
MauricioGiacomello <giacomerro@gmail.com>
2023-04-05 21:40:55 -0300

    Primeiro commit

---
+++ DOG.BANK/DOG.BANK/Controllers/UsuariosController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using System.Text.Json.Nodes;
+
+namespace DOB.ApiDotNet6.Api.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class UsuariosController : ControllerBase
+    {
+        [HttpGet(Name = "Usuarios")]
+        public string ObterUsuarios() 
+        {
+            var usuario = "Mauricio de Souza";
+
+            return usuario;
+        }
+    }
+}
